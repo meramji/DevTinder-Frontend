25fed835d74d3dc2490df1097467f5b00fcafb8f
ram ji <ramjigupta2707@gmail.com>
2025-11-11 00:09:05 +0530

    Added Routeing and Footer

---
+++ src/Body.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router";
+import Footer from "./Footer";
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Navbar.jsx
@@ -0,0 +1,44 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="navbar bg-base-300 shadow-sm w-full  mx-4 my-4 px-4 rounded-xl">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl hover:bg-base-200">DevTinder</a>
+        </div>
+
+        <div className="flex gap-2">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar hover:scale-105 transition-transform duration-200"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="User avatar"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><a>Settings</a></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;

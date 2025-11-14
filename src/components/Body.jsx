714110bc710658e3e7488118f7e3b7d86f52a240
ram ji <ramjigupta2707@gmail.com>
2025-11-14 22:27:12 +0530

    Fixing error in Login of DevTinder

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,36 @@
 import Navbar from "./Navbar";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import Footer from "./Footer";
+import { Base_url } from "../Utils/constants";
+import { useDispatch } from "react-redux";
+import { addusers } from "../Utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchuser = async () => {
+    try {
+      const res = await axios.get(Base_url + "/profile/view", {
+        withCredentials: true,
+      });
+      // console.log(res);
+
+      dispatch(addusers(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      } else {
+        console.log(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchuser();
+  }, []);
+
   return (
     <div>
       <Navbar></Navbar>

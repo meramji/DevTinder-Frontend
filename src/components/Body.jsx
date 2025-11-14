b334b8e0d739ad680e3b7393ac1031dbd7608aa4
ram ji <ramjigupta2707@gmail.com>
2025-11-15 00:37:55 +0530

    Bug Fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,34 +2,42 @@ import Navbar from "./Navbar";
 import { Outlet, useNavigate } from "react-router";
 import Footer from "./Footer";
 import { Base_url } from "../Utils/constants";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addusers } from "../Utils/userSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Body = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const userdata=useSelector(store=>store.user);
+
   const fetchuser = async () => {
     try {
       const res = await axios.get(Base_url + "/profile/view", {
         withCredentials: true,
       });
-      // console.log(res);
-
       dispatch(addusers(res.data));
+      // setdata(false);
     } catch (err) {
       if (err.status === 401) {
         navigate("/login");
       } else {
         console.log(err);
       }
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchuser();
   }, []);
+
+  if (loading === true) {
+    return <div></div>;
+  }
 
   return (
     <div>
